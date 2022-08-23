16321894eba55c9a4f5e52ee8d5225104da726b2
Hauke Schnau <hauke@schnau-lilienthal.de>
2022-08-23 18:02:13 +0200

    disable debug logs

--- public/send_message.php
+++ public/send_message.php
@@ -20,7 +20,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	try {
 		//Server settings
-		$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 		$mail->isSMTP();                                            //Send using SMTP
 		$mail->Host       = getenv("SMTP_HOST");                     //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
